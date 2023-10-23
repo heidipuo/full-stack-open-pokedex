51c28f218bcdabd3de18e6a37533bebb0e3ca5b6
Heidi <puotiniemi.heidi@gmail.com>
2023-10-23 16:10:45 +0300

    11.9: extended cypress test

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -5,4 +5,14 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('one can navigate from main page to a pokemon page', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('overgrow')
+    cy.contains('Next').click()
+    cy.contains('venusaur')
+  })
 })
+
+
