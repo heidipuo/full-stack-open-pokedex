d0a79e9e913e9c235cc82c24957237612f7bd0a1
Heidi <puotiniemi.heidi@gmail.com>
2023-10-24 11:39:56 +0300

    11.10 deploy to prod

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -6,7 +6,7 @@ describe('Pokedex', function() {
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 
-  it('one can navigate from main page to a pokemon page', function() {
+  it('one can navigate in the page', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur').click()
     cy.contains('overgrow')
