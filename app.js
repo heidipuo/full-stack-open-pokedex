8fa98378e8e413a7d5bf26db9e88ae5d93a0136c
Heidi <puotiniemi.heidi@gmail.com>
2023-10-24 13:02:58 +0300

    11.10.6: added health check and rollback

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
+
 
 app.get('/health', (req, res) => {
-  throw 'error...'
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
+  console.log('version')
   res.send('7')
 })
+
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line
